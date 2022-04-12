f27b9515e2c38c4ed0f571dd98b118e6311af34b
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-04-12 23:27:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// VANTA.FOG({
+// 	el: "#intro",
+// 	minHeight: 200.00,
+// 	minWidth: 200.00,
+// 	highlightColor: 0xffffff,
+// 	midtoneColor: 0x0,
+// 	lowlightColor: 0x0,
+// 	baseColor: 0x0,
+// 	blurFactor: 0.23,
+// 	zoom: 1.80
+// });
+
+VANTA.TOPOLOGY({
+	el: "#intro",
+	mouseControls: false,
+	touchControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	scale: 2.00,
+	scaleMobile: 0.60,
+	color: 0xfff00,
+	backgroundColor: 0x171818
+ });
+VANTA.FOG({
+	el: "#portfolio",
+	highlightColor: 0xff00,
+	midtoneColor: 0x337533,
+	lowlightColor: 0x0,
+	baseColor: 0x0,
+	blurFactor: 0.29,
+	speed: 1.40,
+	zoom: 1.90
+});
+
