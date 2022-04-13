a5a913e7226b89b730d6d23ec7e91845cfec551f
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-04-14 00:56:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,14 +8,21 @@ function menu() {
 
 document.querySelector(".theme-btn").addEventListener("click", themeSwitcer);
 function themeSwitcer() {
-	let theme = document.querySelector(".theme-btn");
-	theme.classList.toggle("theme-btn__active");
+	let themeBtn = document.querySelector(".theme-btn");
+	themeBtn.classList.toggle("theme-btn__active");
+	let theme = document.querySelector(".theme");
+	theme.classList.toggle("theme__dark");
+	theme.classList.toggle("theme__light");
+
+	let fog = document.querySelector(".portfolio");
+	fog.classList.toggle("fog__dark");
+	fog.classList.toggle("fog__light");
 }
 
 
 
 VANTA.FOG({
-	el: "#portfolio",
+	el: ".fog__dark",
 	highlightColor: 0x0,
 	midtoneColor: 0x21fd7e,
 	lowlightColor: 0x0,
@@ -23,9 +30,19 @@ VANTA.FOG({
 	blurFactor: 0.29,
 	speed: 1.50
 });
+VANTA.FOG({
+	el: ".fog__light",
+	highlightColor: 0x35b924,
+	midtoneColor: 0x29be29,
+	lowlightColor: 0xffffff,
+	baseColor: 0xffffff,
+	blurFactor: 0.31,
+	speed: 1.50,
+	zoom: 2.70
+});
 
 VANTA.FOG({
-	el: "#feedback",
+	el: ".feedback",
 	highlightColor: 0x0,
 	midtoneColor: 0x21fd7e,
 	lowlightColor: 0x0,
