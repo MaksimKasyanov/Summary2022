8127c6aab45857798cb7e9495ba4951b4adf1dfe
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-04-15 17:14:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,34 +20,3 @@ function themeSwitcer() {
 }
 
 
-
-VANTA.FOG({
-	el: ".fog__dark",
-	highlightColor: 0x0,
-	midtoneColor: 0x21fd7e,
-	lowlightColor: 0x0,
-	baseColor: 0x0,
-	blurFactor: 0.29,
-	speed: 1.50
-});
-VANTA.FOG({
-	el: ".fog__light",
-	highlightColor: 0x35b924,
-	midtoneColor: 0x29be29,
-	lowlightColor: 0xffffff,
-	baseColor: 0xffffff,
-	blurFactor: 0.31,
-	speed: 1.50,
-	zoom: 2.70
-});
-
-VANTA.FOG({
-	el: ".feedback",
-	highlightColor: 0x0,
-	midtoneColor: 0x21fd7e,
-	lowlightColor: 0x0,
-	baseColor: 0x0,
-	blurFactor: 0.29,
-	speed: 1.50
-});
-
