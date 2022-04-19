5c3a32ace321015812becbe39b9b3b0278a3eba7
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-04-19 22:47:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,11 +13,30 @@ function themeSwitcer() {
 	let theme = document.querySelector(".theme");
 	theme.classList.toggle("theme__dark");
 	theme.classList.toggle("theme__light");
-
-	let fog = document.querySelector(".portfolio");
-	fog.classList.toggle("fog__dark");
-	fog.classList.toggle("fog__light");
 }
+
+
+const swiper = new Swiper('.portfolio__slider', {
+	loop: true,
+	effect: 'fade',
+	spaceBetween: 30,
+	pagination: {
+		el: '.portfolio__bullets',
+		clickable: true,
+		slideClass: 'portfolio__slide',
+		bulletClass: 'portfolio__bullet',
+		bulletActiveClass: 'portfolio__bullet-active',
+	},
+});
+
+
+
+
+// document.querySelectorAll(".intro-btn").addEventListener("click", introContacts);
+// function introContacts() {
+// 	let introBtn = document.querySelector(".intro-btn__item");
+// 	introBtn.classList.toggle("intro-btn__item_active");
+// }
 
 
 // document.querySelector(".tile").addEventListener("click", tile);
