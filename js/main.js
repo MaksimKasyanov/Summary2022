ee4e86ad72597c00e0f2cd26397b0b4b24174dc8
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-04-28 20:01:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -49,10 +49,17 @@ function closeFormEmployer() {
 	feedbackBtn.classList.remove("form__employer_active");
 }
 
+document.querySelector(".lang-btn").addEventListener("click", switchLang);
+function switchLang() {
+	let switchLangEn = document.querySelector(".lang-btn");
+	switchLangEn.classList.toggle("lang-btn_active");
+}
+
 const swiper = new Swiper('.portfolio__slider', {
 	loop: true,
 	effect: 'fade',
 	spaceBetween: 30,
+	allowTouchMove: false,
 	pagination: {
 		el: '.portfolio__bullets',
 		clickable: true,
@@ -62,20 +69,3 @@ const swiper = new Swiper('.portfolio__slider', {
 	},
 });
 
-
-
-
-// document.querySelectorAll(".intro-btn").addEventListener("click", introContacts);
-// function introContacts() {
-// 	let introBtn = document.querySelector(".intro-btn__item");
-// 	introBtn.classList.toggle("intro-btn__item_active");
-// }
-
-
-// document.querySelector(".tile").addEventListener("click", tile);
-// function tile() {
-// 	this.document.querySelector(".burger");
-// 	burger.classList.toggle("burger__cross");
-// 	let menu = document.querySelector(".menu");
-// 	menu.classList.toggle("menu__active");
-// }
